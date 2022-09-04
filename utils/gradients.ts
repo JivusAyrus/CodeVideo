9117a87d77163e8959d8dba5c12c5a6dee48c187
Suvij Surya <suvijsurya76@gmail.com>
2022-09-04 20:12:04 +0530

    Add gradients and supported languages

---
+++ utils/gradients.ts
@@ -0,0 +1,100 @@
+import { Gradient } from "./interfaces";
+
+export const gradients: Gradient[] = [
+  {
+    id: 1,
+    angle: 90,
+    values: [0, "#D397FA", 0.0001, "#D397FA", 1, "#8364E8"],
+    cssString:
+      "linear-gradient(90deg, #D397FA 0%, #D397FA 0.01%, #8364E8 100%)",
+  },
+  {
+    id: 2,
+    angle: 90,
+    values: [0.0001, "#FFAFBD", 1, "#FFC3A0"],
+    cssString: "linear-gradient(90deg, #FFAFBD 0.01%, #FFC3A0 100%)",
+  },
+  {
+    id: 3,
+    angle: 90,
+    values: [0.0001, "#8879B2", 1, "#EAAFC8"],
+    cssString: "linear-gradient(90deg, #8879B2 0.01%, #EAAFC8 100%)",
+  },
+  {
+    id: 4,
+    angle: 90,
+    values: [0.0001, "#8BC6EC", 1, "#9599E2"],
+    cssString: "linear-gradient(90deg, #8BC6EC 0.01%, #9599E2 100%)",
+  },
+  {
+    id: 5,
+    angle: 43.58,
+    values: [0.424, "#FBDA61", 0.9792, "#FF5ACD"],
+    cssString: "linear-gradient(43.58deg, #FBDA61 4.24%, #FF5ACD 97.92%)",
+  },
+  {
+    id: 6,
+    angle: 180,
+    values: [0.0001, "#A9C9FF", 1, "#FFBBEC"],
+    cssString: "linear-gradient(180deg, #A9C9FF 0.01%, #FFBBEC 100%)",
+  },
+  {
+    id: 7,
+    angle: 226.32,
+    values: [0.0001, "#FF3CAC", 0.524, "#784BA0", 1, "#2B86C5"],
+    cssString:
+      "linear-gradient(226.32deg, #FF3CAC -25.84%, #784BA0 40.09%, #2B86C5 100%)",
+  },
+  {
+    id: 8,
+    angle: 47.5,
+    values: [0, "#74EBD5", 0.96, "#9FACE6"],
+    cssString: "linear-gradient(47.5deg, #74EBD5 0%, #9FACE6 96%)",
+  },
+  {
+    id: 9,
+    angle: 46.2,
+    values: [0, "#85FFBD", 0.9802, "#FFFED3"],
+    cssString: "linear-gradient(46.2deg, #85FFBD 0%, #FFFED3 98.02%)",
+  },
+  {
+    id: 10,
+    angle: 42.22,
+    values: [0.278, "#FBAB7E", 0.9837, "#F7CE68"],
+    cssString: "linear-gradient(42.22deg, #FBAB7E 2.78%, #F7CE68 98.37%)",
+  },
+  {
+    id: 11,
+    angle: 90,
+    values: [0.0001, "#43CEA2", 1, "#548AC0"],
+    cssString: "linear-gradient(90deg, #43CEA2 0.01%, #548AC0 100%)",
+  },
+  {
+    id: 12,
+    angle: 226.32,
+    values: [
+      0.0001,
+      "#FFCC70",
+      0.0812,
+      "#F6B97C",
+      0.5829,
+      "#CE74C8",
+      1,
+      "#2B86C5",
+    ],
+    cssString:
+      "linear-gradient(226.32deg, #FFCC70 -25.84%, #F6B97C -15.62%, #CE74C8 47.51%, #2B86C5 100%)",
+  },
+  {
+    id: 13,
+    angle: 43,
+    values: [0, "#4158d0", 0.46, "#c850c0", 1, "#ffcc70"],
+    cssString: "linear-gradient(43deg,#4158d0 0,#c850c0 46%,#ffcc70 100%)",
+  },
+  {
+    id: 14,
+    angle: 160,
+    values: [0, "#0093e9", 1, "#80d0c7"],
+    cssString: "linear-gradient(160deg,#0093e9 0,#80d0c7 100%)",
+  },
+];
