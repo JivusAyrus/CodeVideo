eced920e0f2e7e2363ca7c8dfdd2ee1ced84a79c
Suvij Surya <suvijsurya76@gmail.com>
2022-09-04 20:07:53 +0530

    Preload roboto mono font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,24 @@
 import Head from 'next/head';
+import { RecoilRoot } from 'recoil';
 import { withWunderGraph } from '../components/generated/nextjs';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
 	return (
-    <>
+    <RecoilRoot>
       <Head>
         <title>Code Video</title>
         <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="preload"
+          href="/fonts/RobotoMono.ttf"
+          as="font"
+          type="font/ttf"
+          crossOrigin="anonymous"
+        />
       </Head>
       <Component {...pageProps} />
-    </>
+    </RecoilRoot>
   );
 }
 
